993a7f0a304e915938725b3274b1bdf9ea9c6430
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-11-11 00:22:54 +0100

    mrs on experiment and factory

--- src/multirobotix/experiment/experiment.py
+++ src/multirobotix/experiment/experiment.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from typing import List
 
 from multirobotix.experiment.scenario import Scenario
@@ -8,18 +9,25 @@ class Experiment:
     We know in a robotic experiment a robot tries to achieve a initial_mission
     """
 
-    def __init__(self, learning_scenarios: List[Scenario], testing_scenarios: List[Scenario]) -> None:
-        self._testing_scenarios = testing_scenarios
-        self._learning_scenarios = learning_scenarios
+    class Experiment(ABC):
+        """
+        We know in a robotic experiment a robot tries to achieve a initial_mission
+        """
 
-        # do the experiment
-        self.learn()
-        self.test()
+        def __init__(self, learning_scenarios: List[Scenario], testing_scenarios: List[Scenario]) -> None:
+            self._testing_scenarios = testing_scenarios
+            self._learning_scenarios = learning_scenarios
 
-    def learn(self) -> None:
-        for scenario in self._learning_scenarios:
-            scenario.learn()
+        def get_learning_scenarios(self) -> List[Scenario]:
+            return self._learning_scenarios
 
-    def test(self) -> None:
-        for scenario in self._testing_scenarios:
-            scenario.test()
+        def get_testing_scenarios(self) -> List[Scenario]:
+            return self._testing_scenarios
+
+        def run_learning_scenarios(self):
+            for learning_scenario in self._learning_scenarios:
+                learning_scenario.learn()
+
+        def run_testing_scenarios(self):
+            for testing_scenario in self._testing_scenarios:
+                testing_scenario.run()
