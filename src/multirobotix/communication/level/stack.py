f07e7486c7997e7b14e47dfed4258177f6d7e9c4
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2026-01-26 20:38:49 +0100

    i dont know

--- src/multirobotix/communication/level/stack.py
+++ src/multirobotix/communication/level/stack.py
@@ -1,4 +1,4 @@
 class Stack:
     def __init__(self, levels:List[Level]):
-        """As the current_level increases from 0 to higher more meaningful but less details pair_set will be avasilable"""
+        """As the current_level increases from 0 to higher more meaningful but less details group will be avasilable"""
         pass
